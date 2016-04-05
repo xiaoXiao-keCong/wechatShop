ca044dd80624808dff0bae4b29bc01f6b762671e
tanruqing <317992262@qq.com>
2016-04-05 23:23:02 +0800

    提交设置界面（未完成）

--- src/js/setting.js
+++ src/js/setting.js
@@ -5,4 +5,18 @@ angular.module('setting', []).controller('settingCtrl', function ($scope) {
     $scope.toNeedKnow = function () {
     	console.log('need-to-know is clicking');
     };
+    $scope.AboutUs = function () {
+    	console.log('AboutUs is clicking');
+    };
+    $scope.Mark = function () {
+    	console.log('Mark is clicking');
+    };
+    $scope.Suggest = function () {
+    	console.log('Suggest is clicking');
+    };
 });
+
+function cacheValue() {
+	var str = "5.00M";
+	return str;
+}
