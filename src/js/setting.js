f7a7b8a75c62b46cd32a3a263b80cf033f337c31
tanruqing <317992262@qq.com>
2016-04-06 23:07:02 +0800

    提交设置界面，完成90%

--- src/js/setting.js
+++ src/js/setting.js
@@ -14,9 +14,24 @@ angular.module('setting', []).controller('settingCtrl', function ($scope) {
     $scope.Suggest = function () {
     	console.log('Suggest is clicking');
     };
+    $scope.ClearCache = function () {
+        console.log('ClearCache is clicking');
+    };
 });
 
-function cacheValue() {
-	var str = "5.00M";
-	return str;
+function getCacheValue() {
+    var str = "5.00M";
+    document.getElementById('cacheValue').innerHTML = str;
 }
+
+function versionInfo() {
+    var str = "V5.0.0";
+    document.getElementById('version').innerHTML = str;
+}
+
+function quitClick() {
+    console.log('quit click');
+}
+
+getCacheValue();
+versionInfo();
