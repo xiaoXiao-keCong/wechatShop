c59917c55ee6a1ce25949452b786c0d1105110e9
trhuo <891567977@qq.com>
2016-04-10 01:56:39 +0800

    完成登录、快速登录、忘记密码前端界面（未实现js逻辑）

---
+++ src/js/forget_pwd.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hugotan on 2016/4/10.
+ */
+angular.module('forgetPwd', []).controller('forgetPwdCtrl', function ($scope) {
+
+});
---
+++ src/js/login.js
@@ -0,0 +1,6 @@
+/**
+ * Created by hugotan on 2016/4/10.
+ */
+angular.module('login', []).controller('loginCtrl', function ($scope) {
+
+});
