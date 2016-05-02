839d87cfa912c6697787d9921247d2853003c2e6
trhuo <891567977@qq.com>
2016-05-03 00:17:24 +0800

    发型师评价完成20%

---
+++ src/js/stylist_comment.js
@@ -0,0 +1,13 @@
+/**
+ * Created by hugotan on 2016/5/2.
+ */
+angular.module('stylistComment', []).controller('stylistCommentCtrl', ['$scope', '$http', function ($scope, $http) {
+	var transFn = function(data) {
+                return $.param(data);
+        },
+        postCfg = {
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            transformRequest: transFn
+        };
+	
+}]);
