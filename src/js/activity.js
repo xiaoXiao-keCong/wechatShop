90a665427bd437541276f14eae562aad3ea1cc49
trhuo <891567977@qq.com>
2016-06-01 00:36:02 +0800

    使用最新活动接口

--- src/js/activity.js
+++ src/js/activity.js
@@ -1,6 +1,14 @@
 /**
  * Created by hugotan on 2016/5/4.
  */
-index.controller('activityCtrl', ['$scope', '$http', function ($scope, $http) {
-	
+index.controller('activityCtrl',
+	['$scope', '$http', '$location', function ($scope, $http, $location) {
+	// 获取所有活动
+	$http.post('/activity/getallactivity.json', postCfg)
+	.success(function (data) {
+		console.log(data);
+	})
+	.error(function (resp) {
+		console.log(resp);
+	});
 }]);
